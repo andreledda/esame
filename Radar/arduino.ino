888aacc37e5cd8b67e35e3697abc73e020f96a10
andreledda <andrea.ledda.no@gmail.com>
2025-04-06 20:30:51 +0200

    Creazione primo prototipo di radar

---
+++ Radar/arduino.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+// variabili servo
+Servo myservo;  // definisci il servo
+int pos = 0;    // posizione servo
+
+// variabili sensore ultrasuoni
+const int trigPin = 9;
+const int echoPin = 10;
+float duration;
+float distance;
+
+int misuraDistanza() {
+    digitalWrite(trigPin, LOW); // stringa di inizializzazione
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);	// impulso
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW); // fine impulso
+    duration = pulseIn(echoPin, HIGH); // Misura per quanto tempo echoPin rimane HIGH, quindi durata del ritorno
+    distanza = (duration * 0.034) / 2;
+    delay(100);
+    Serial.print(pos);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+}
+
+void setup() {
+  myservo.attach(9);  // pin di connessione allla scheda
+  Serial.begin(9600)
+}
+
+void loop() {
+  for (pos = 15; pos <= 165; pos += 1) { // imposta pos a 0, poi aumentalo unità per unità. Dopo ogni aumento, esegui il codice nel ciclo fino a quando pos arriva a 180
+    myservo.write(pos);              // sposta il servo alla posizione pos
+    delay(15);                       // tempo per far muovere il servo
+    misuraDistanza()
+  }
+  for (pos = 165; pos >= 15; pos -= 1) { // imposta pos a 0, poi aumentalo unità per unità. Dopo ogni aumento, esegui il codice nel ciclo fino a quando pos arriva a 0
+    myservo.write(pos);              // sposta il servo alla posizione pos
+    delay(15);                       // tempo per far muovere il servo
+    misuraDistanza()
+  }
+}
