a193cd00e4e7f330b008eab7e5a509b0f2328cb7
andreledda <andrea.ledda.no@gmail.com>
2025-04-11 17:58:57 +0200

    Compilazione via Arduino IDE

--- Radar/arduino.ino
+++ Radar/arduino.ino
@@ -5,10 +5,11 @@ Servo myservo;  // definisci il servo
 int pos = 0;    // posizione servo
 
 // variabili sensore ultrasuoni
-const int trigPin = 9;
+const int trigPin = 9; 
 const int echoPin = 10;
 float duration;
 float distance;
+
 
 int misuraDistanza() {
     digitalWrite(trigPin, LOW); // stringa di inizializzazione
@@ -17,7 +18,7 @@ int misuraDistanza() {
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW); // fine impulso
     duration = pulseIn(echoPin, HIGH); // Misura per quanto tempo echoPin rimane HIGH, quindi durata del ritorno
-    distanza = (duration * 0.034) / 2;
+    distance = (duration * 0.034) / 2;
     delay(100);
     Serial.print(pos);
     Serial.print(",");
@@ -26,19 +27,19 @@ int misuraDistanza() {
 }
 
 void setup() {
-  myservo.attach(9);  // pin di connessione allla scheda
-  Serial.begin(9600)
+  myservo.attach(9);  // pin di connessione alla scheda
+  Serial.begin(9600);
 }
 
 void loop() {
   for (pos = 15; pos <= 165; pos += 1) { // imposta pos a 0, poi aumentalo unità per unità. Dopo ogni aumento, esegui il codice nel ciclo fino a quando pos arriva a 180
     myservo.write(pos);              // sposta il servo alla posizione pos
     delay(15);                       // tempo per far muovere il servo
-    misuraDistanza()
+    misuraDistanza();
   }
   for (pos = 165; pos >= 15; pos -= 1) { // imposta pos a 0, poi aumentalo unità per unità. Dopo ogni aumento, esegui il codice nel ciclo fino a quando pos arriva a 0
     myservo.write(pos);              // sposta il servo alla posizione pos
     delay(15);                       // tempo per far muovere il servo
-    misuraDistanza()
+    misuraDistanza();
   }
 }
