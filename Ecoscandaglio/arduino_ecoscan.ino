f35f45909f5018584bd8f8fb4142e252771460d1
andreledda <andrea.ledda.no@gmail.com>
2025-04-20 19:52:48 +0200

    Update arduino_ecoscan.ino

--- Ecoscandaglio/arduino_ecoscan.ino
+++ Ecoscandaglio/arduino_ecoscan.ino
@@ -1,1 +1,26 @@
+const int trigPin = 9;
+const int echoPin = 10;
 
+
+float duration; // tempo tra invio e ritorno del segnale
+float distance; // distanza dell'oggetto
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+    digitalWrite(trigPin, LOW); // stringa di inizializzazione
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);	// impulso
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW); // fine impulso
+    duration = pulseIn(echoPin, HIGH); // Misura per quanto tempo echoPin rimane HIGH, quindi durata del ritorno
+    distance = (duration * 0.034) / 2;
+    Serial.println(distance);
+    delay(100);
+}
